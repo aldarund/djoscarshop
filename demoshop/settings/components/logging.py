6a8cdbf381d9065f9a9b88dc87562cb92b9d0c84
Alexander Moiseenko <imdagger@yandex.ru>
2015-02-07 04:02:38 +0300

    enabled sentry

--- demoshop/settings/components/logging.py
+++ demoshop/settings/components/logging.py
@@ -1,1 +1,2 @@
-#http://f291317d4ced48dc9ce2b6db9176fc13:ffb847cb45b54a13a82445178f74f40f@sentry.test.viewdev.com/4
+SENTRY_DSN = http://f291317d4ced48dc9ce2b6db9176fc13:ffb847cb45b54a13a82445178f74f40f@sentry.test.viewdev.com/4
+SENTRY_AUTO_LOG_STACK = True
