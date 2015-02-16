a5b788da899a04bf99285b81ba6c887278c55bd9
Alexander Moiseenko <imdagger@yandex.ru>
2015-02-16 18:48:59 +0300

    display and process product attributes

---
+++ demoshop/basket/templatetags/product_variants.py
@@ -0,0 +1,22 @@
+import json
+from django import template
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.safestring import mark_safe
+from oscar.apps.catalogue.models import *
+
+register = template.Library()
+
+@register.assignment_tag()
+def get_variant_options(code):
+    attr = ProductAttribute.objects.filter(option_group__isnull=False).get(code=code)
+    return {
+        'group': attr,
+        'options': attr.option_group.options.all(),
+    }
+
+@register.filter()
+def get_variant_values(product, attr_name):
+    if product.children:
+        return [getattr(child.attr, attr_name) for child in product.children.all()]
+    else:
+        return [getattr(product.attr, attr_name)]
